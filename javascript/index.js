693bf1c49b5fec726cd87ef2e6abe0b4c51821ca
Constanza Galía Maldonado <coco.galiam@gmail.com>
2022-08-23 19:09:06 -0300

    JS

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+// const nombre = 'Constanza';
+// const dni = 37497308;
+// const fullStack = true;
+// const sexo = 'Femenino'
+
+// console.log(!fullStack);
+
+let index = 1;
+index = ++index;
+index = --index;
+
+let dni = true;
+let edad = 15;
+
+
+const entrarAlBoliche = dni && edad > 18;
+console.log(entrarAlBoliche);
