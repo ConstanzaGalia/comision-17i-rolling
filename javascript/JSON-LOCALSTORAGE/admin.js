2300a7b0c38705daa515815f0f3d0f124f47c994
Constanza Galía Maldonado <coco.galiam@gmail.com>
2022-09-27 21:35:09 -0300

    Terminado CRUD y buscador

--- javascript/JSON-LOCALSTORAGE/admin.js
+++ javascript/JSON-LOCALSTORAGE/admin.js
@@ -4,7 +4,9 @@ const tablaProductos = document.getElementById('TablaProductos');
 const formProductos = document.getElementById('FormProductos');
 const inputImgProductos = document.getElementById('InputImgProductos');
 const inputNameProductos = document.getElementById('InputNameProductos');
-const inputDescripcionProductos = document.getElementById('InputDescripcionProductos');
+const inputDescripcionProductos = document.getElementById(
+  'InputDescripcionProductos'
+);
 const inputPrecioProductos = document.getElementById('InputPrecioProductos');
 
 //INFO LOCAL STORAGE
@@ -47,31 +49,85 @@ showAdminName.innerHTML = `
     <button type="button" class="dropdown-item" onclick="logOutAdmin()">Cerrar Sesión</button>
   </div>
 </div>
+
+<!-- Modal Editar Productos -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Editar Producto</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form id="FormEditProductos">
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Imágen</label>
+              <input type="text" class="form-control" id="InputImgProductosE" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Nombre</label><span class="text-danger">*</span>
+              <input type="text" class="form-control" id="InputNameProductosE" aria-describedby="emailHelp" required>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Descripción</label>
+              <input type="text" class="form-control" id="InputDescripcionProductosE" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Precio</label><span class="text-danger">*</span>
+              <input type="texto" class="form-control" id="InputPrecioProductosE" aria-describedby="emailHelp" autocomplete="off" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Editar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 `;
+
+// FORM EDITAR PRODUCTOS
+const formProductosE = document.getElementById('FormEditProductos');
+const inputImgProductosE = document.getElementById('InputImgProductosE');
+const inputNameProductosE = document.getElementById('InputNameProductosE');
+const inputDescripcionProductosE = document.getElementById(
+  'InputDescripcionProductosE'
+);
+const inputPrecioProductosE = document.getElementById('InputPrecioProductosE');
+
 //FUNCIONES
 const createProducts = (productos) => {
-  tablaProductos.innerHTML = productos.map((producto, index) => (
-    `
+  tablaProductos.innerHTML = productos
+    .map(
+      (producto, index) =>
+        `
     <tr>
       <td>${++index}</td>
-      <td class="w-25"><img src="${producto.img}" alt="${producto.name}" class="w-25"/></td>
+      <td class="w-25"><img src="${producto.img}" alt="${
+          producto.name
+        }" class="w-25"/></td>
       <td>${producto.name}</td>
       <td>${producto.descripcion}</td>
       <td>${producto.precio}</td>
       <td>
-      <button type="button" class="btn btn-sm btn-warning text-light" data-toggle="modal" data-target="#modalEditProduct">
+      <button type="button" onclick="uploadFormEditProduct(${producto.id})" class="btn btn-sm btn-warning text-light" data-bs-toggle="modal" data-bs-target="#exampleModal">
       <i class="fas fa-user-edit"></i></button>
-      <button class="btn btn-sm btn-danger">
+      <button type="button" onclick="deleteProduct(${
+        producto.id
+      })" class="btn btn-sm btn-danger">
       <i class="fas fa-trash-alt"></i></button>
       </td>
     </tr>
   `
-  ))
+    )
+    .join('');
 };
 
 const displayProducts = () => {
-  createProducts(productos);
-}
+  const productosLocalStorage = JSON.parse(localStorage.getItem('productos')) || [];
+  const productsAvailable = productosLocalStorage.filter(
+    (producto) => !producto.hasOwnProperty('deleteAt')
+  );
+  createProducts(productsAvailable);
+};
 displayProducts();
 
 formProductos.onsubmit = (event) => {
@@ -82,18 +138,54 @@ formProductos.onsubmit = (event) => {
   const descripcion = inputDescripcionProductos.value;
   const precio = inputPrecioProductos.value;
 
-    productos.push({
-      id: idRandom(),
-      img,
-      name,
-      descripcion,
-      precio,
-    });
+  productos.push({
+    id: idRandom(),
+    img,
+    name,
+    descripcion,
+    precio,
+  });
 
-    localStorage.setItem('productos', JSON.stringify(productos));
-    swal('Producto guardado con éxito', 'success');
-    formProductos.reset();
-    displayProducts();
-    let modal = bootstrap.Modal.getInstance(AgregarProducto);
-    modal.hide();
+  localStorage.setItem('productos', JSON.stringify(productos));
+  swal('Producto guardado con éxito', 'success');
+  formProductos.reset();
+  displayProducts();
+  bootstrap.Modal.getInstance(AgregarProducto).hide();
+};
+
+const deleteProduct = (id) => {
+  const updateProducts = productos.map((producto) =>
+    producto.id === id ? { ...producto, deleteAt: new Date() } : producto
+  );
+  localStorage.setItem('productos', JSON.stringify(updateProducts));
+  displayProducts();
+};
+
+let idProductEdit;
+const uploadFormEditProduct = (id) => {
+  const producto = productos.find(producto => producto.id === id);
+  inputImgProductosE.value = producto.img;
+  inputNameProductosE.value = producto.name;
+  inputDescripcionProductosE.value = producto.descripcion;
+  inputPrecioProductosE.value = producto.precio;
+  idProductEdit = id;
+};
+
+formProductosE.onsubmit = (event) => {
+  event.preventDefault();
+
+  const img = inputImgProductosE.value;
+  const name = inputNameProductosE.value;
+  const descripcion = inputDescripcionProductosE.value;
+  const precio = inputPrecioProductosE.value;
+
+  const updateProducts = productos.map((producto) =>
+    producto.id === idProductEdit ? { ...producto, img, name, descripcion, precio } : producto
+  );
+
+  localStorage.setItem('productos', JSON.stringify(updateProducts));
+  swal('Producto editado con éxito', 'success');
+  formProductosE.reset();
+  displayProducts();
+  bootstrap.Modal.getInstance(exampleModal).hide();
 };
--- javascript/JSON-LOCALSTORAGE/index.js
+++ javascript/JSON-LOCALSTORAGE/index.js
@@ -9,7 +9,7 @@ const pAlreadyEmail = document.getElementById('alreadyEmail');
 const pPassInvalid = document.getElementById('passValid');
 
 //REGEX
-const regexPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+const regexPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
 //FORMULARIO DE LOGIN
 const formLogin = document.getElementById('FormularioLogin');
@@ -17,13 +17,18 @@ const inputEmailLogin = document.getElementById('InputEmailLogin');
 const inputPassLogin = document.getElementById('InputPasswordLogin');
 const modalLogin = document.getElementById('Login');
 const pUserNotExist = document.getElementById('noExiste');
+const formSearch = document.getElementById('formSearch');
+const inputSearch = document.getElementById('inputSearch');
 
 //INFO LOCAL STORAGE
 const users = JSON.parse(localStorage.getItem('users')) || [];
 const userLogged = JSON.parse(localStorage.getItem('userLogged'));
+const productos = JSON.parse(localStorage.getItem('productos')) || [];
 
 //HTML
 const divShowbtnNavbar = document.querySelector('#divShowbtnNavbar');
+const divContainerProductos = document.querySelector('#ContainerProductos');
+
 if (!userLogged) {
   divShowbtnNavbar.innerHTML = `
   <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Login">
@@ -44,7 +49,7 @@ if (!userLogged) {
     <button type="button" class="dropdown-item" onclick="logOut()">Cerrar Sesión</button>
   </div>
 </div>
-  `
+  `;
 }
 
 //FUNCIONES
@@ -62,7 +67,7 @@ formRegistro.onsubmit = (event) => {
   if (!regexPass.test(pass)) {
     console.log(regexPass.test(pass));
     pPassInvalid.classList.remove('d-none');
-    return
+    return;
   }
 
   const findUser = users.find((user) => user.email === email);
@@ -116,11 +121,48 @@ const redirect = (url) => {
   setTimeout(() => {
     window.location.href = url;
   }, 1000);
-}
+};
 
 const logOut = () => {
   localStorage.removeItem('userLogged');
   setTimeout(() => {
     location.reload();
-  }, 1000)
+  }, 1000);
+};
+
+const displayProducts = (productos) => {
+  const productsAvailable = productos.filter(
+    (producto) => !producto.hasOwnProperty('deleteAt')
+  );
+  divContainerProductos.innerHTML = productsAvailable
+    .map(
+      (producto) =>
+        `
+        <div class="card" style="width: 18rem;">
+        <img src="${producto.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${producto.name}</h5>
+          <p class="card-text">${producto.descripcion}
+          <span class="badge ${producto.precio < 3000 ? 'bg-success' : 'bg-danger'} ">$ ${producto.precio}</span>
+          </p>
+          <a href="#" class="btn btn-primary">Comprar</a>
+        </div>
+      </div>
+  `
+    )
+    .join('');
+};
+displayProducts(productos);
+
+formSearch.onsubmit = (e) => {
+  e.preventDefault();
+  const term = inputSearch.value;
+  const searchProducts = productos.filter(producto => 
+    producto.name.toLowerCase().includes(term.toLowerCase())
+  );
+  displayProducts(searchProducts);
+}
+
+const clearSearch = () => {
+  displayProducts(productos);
 }
